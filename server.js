8cfb4aa5b419fff9ea52821e3d8f6c6960dd4517
Dwij-063 <dwijchaudhari2@gmail.com>
2026-05-14 12:41:28 +0530

    final clean structure

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const TaskSchema = new mongoose.Schema({
+    title: String
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+app.get('/tasks', async(req,res)=>{
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+app.post('/tasks', async(req,res)=>{
+    const task = new Task(req.body);
+    await task.save();
+    res.json(task);
+});
+
+app.put('/tasks/:id', async(req,res)=>{
+
+    await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body
+    );
+
+    res.json({
+        message:"Updated"
+    });
+});
+
+app.delete('/tasks/:id', async(req,res)=>{
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({message:"Deleted"});
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log("Server Running");
+});
